f62635ce42ecf84e0660f5322f7f2c5b542d0185
Bharathi Balasubramanyam <beebha@gmail.com>
2013-12-14 15:03:41 -0500

    Documentation

--- grails-app/controllers/edu/harvard/cscie56/finalproject/ForgotController.groovy
+++ grails-app/controllers/edu/harvard/cscie56/finalproject/ForgotController.groovy
@@ -13,20 +13,31 @@ class ForgotController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
+    /**
+     * This method clears any flash messages and redirects to the forgot view
+     */
+
     def index()
     {
         flash.clear()
         render(view: "forgot")
     }
 
+    /**
+     * This method validates that the user exists
+     * and if so resets the password to the new specified password
+     */
+
     def reset()
     {
+        // check that required params username and password present
         if (params.username?.trim() == "" || params.password?.trim() == "") {
             flash.message = "Email and/or password cannot be blank."
             render(view: "forgot", model: [forgotInstance: new User(params)])
             return
         }
 
+        // check that user exists
         def userInstance = User.findByUsername(params.username)
 
         if(!userInstance) {
@@ -35,6 +46,7 @@ class ForgotController {
             return
         }
 
+        // save the new password
         userInstance.properties = params
 
         userInstance.save(flush: true)
@@ -44,11 +56,13 @@ class ForgotController {
             return
         }
 
+        // re-authenticate the user
         if (springSecurityService.loggedIn &&
                 springSecurityService.principal.username == userInstance.username) {
             springSecurityService.reauthenticate userInstance.username
         }
 
+        // set message and render ciew with message
         flash.message = "Password was successfully reset. Please return to the login page and use the new credentials."
         render(view: "forgot", model: [forgotInstance: userInstance])
     }
--- grails-app/controllers/edu/harvard/cscie56/finalproject/HomeController.groovy
+++ grails-app/controllers/edu/harvard/cscie56/finalproject/HomeController.groovy
@@ -15,11 +15,21 @@ class HomeController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
+    /**
+     * This method clears any flash messages and
+     * redirects to the surveys created by logged in user
+     */
+
     def index()
     {
         flash.clear()
         mysurveyindex()
     }
+
+    /**
+     * This method gets all complete, incomplete and published surveys of the logged in user
+     * and then redirects to the my surveys home view
+     */
 
     def mysurveyindex()
     {
@@ -27,10 +37,16 @@ class HomeController {
 
         User user = User.load(springSecurityService.principal.id)
 
+        // get published surveys of logged in user
         def activeCategoriesAndCounts = SurveyUtils.getMyActiveSurveyCategoriesAndCount(user)
+
+        // get completed surveys of logged in user
         def completedCategoriesAndCounts = SurveyUtils.getMyCompletedSurveyCategoriesAndCount(user)
+
+        // get incomplete surveys of logged in user
         def incompleteCategoriesAndCounts = SurveyUtils.getMyIncompleteSurveyCategoriesAndCount(user)
 
+        // redirect to home view
         render (view: 'home', model: [
                 title: 'My Surveys',
                 activeCategoriesAndCounts: activeCategoriesAndCounts,
@@ -40,6 +56,11 @@ class HomeController {
 
     }
 
+    /**
+     * This method gets all published surveys of all users
+     * and then redirects to the all surveys home view
+     */
+
     def allsurveyindex()
     {
         if (params.message != null) {
@@ -48,10 +69,17 @@ class HomeController {
             flash.clear()
         }
 
+        // get all published surveys of all users
         def categoriesAndCounts = SurveyUtils.getAllSurveyCategoriesAndCount()
 
+        // redirect to home view
         render (view: 'home', model: [title: 'All Surveys', categoriesAndCounts: categoriesAndCounts])
     }
+
+    /**
+     * This method gets all survey results of published surveys of the logged in user
+     * and then redirects to the all results home view
+     */
 
     def allresults()
     {
@@ -59,8 +87,10 @@ class HomeController {
 
         User user = User.load(springSecurityService.principal.id)
 
+        // get all survey results of published surveys of logged in user
         def allResultsCategoriesAndCounts = SurveyUtils.getAllSurveyResultsCategoriesAndCountByCreator(user)
 
+        // redirect to home view
         render (view: 'home', model: [title: 'All Results', allResultsCategoriesAndCounts: allResultsCategoriesAndCounts])
     }
 }
